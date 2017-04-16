eea1aa876d7bb4ef3bd393911614e371c375b29a
Dr. Michael Kreim <michael.kreim@da-cons.de>
2017-04-16 13:17:51 +0200

    fixed path in SetSoftLinks.sh

--- SetSoftLinks.sh
+++ SetSoftLinks.sh
@@ -6,8 +6,8 @@
 #         Author:  (2012), Michael Kreim
 #                  http://www.perfect-kreim.de/ michael@perfect-kreim.de
 # =============================================================================
-    ln -f -s ~/config/vimconfig/gvimrc  ~/.gvimrc
-    ln -f -s ~/config/vimconfig/vim/    ~/.vim      
-    ln -f -s ~/config/vimconfig/vimrc   ~/.vimrc
-    ln -f -s ~/config/vimconfig/viminfo ~/.viminfo
+    ln -f -s ~/config/vim8config/gvimrc  ~/.gvimrc
+    ln -f -s ~/config/vim8config/vim/    ~/.vim
+    ln -f -s ~/config/vim8config/vimrc   ~/.vimrc
+    ln -f -s ~/config/vim8config/viminfo ~/.viminfo
 # --- EOF ---------------------------------------------------------------------
