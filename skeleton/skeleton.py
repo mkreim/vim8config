e8e8a3bd467e881acaa02485dfdb5e69d00d0363
Dr. Michael Kreim <mk@netsyno.com>
2019-02-12 13:06:31 +0100

    updated submodules

--- skeleton/skeleton.py
+++ skeleton/skeleton.py
@@ -1,26 +1,42 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 # =========================================================================
 """
-*       Filename:  [:VIM_EVAL:]expand("%:t")[:END_EVAL:] 
+*       Filename:  [:VIM_EVAL:]expand("%:t")[:END_EVAL:]
 *    Description:
 *        Created:  [:VIM_EVAL:]strftime("%c")[:END_EVAL:]
-*         Author:  ([:VIM_EVAL:]strftime('%Y')[:END_EVAL:]), Dr. Michael Kreim michael.kreim@da-cons.de
-*        Contact:  support@da-cons.de
-*        Company:  da-cons GmbH http://www.da-cons.de/"""
+*         Author:  ([:VIM_EVAL:]strftime('%Y')[:END_EVAL:]), Dr. Michael Kreim
+*        Company:  NETSYNO Software GmbH https://www.netsyno.com"""
 # =========================================================================
 # Imports -{{{-------------------------------------------------------------
+import traceback
+import pdb
 import sys
+
 #-}}}----------------------------------------------------------------------
+
 
 # Helping functions -{{{---------------------------------------------------
 def usage():
     print __doc__
+
+
 #-}}}----------------------------------------------------------------------
+
 
 # Main -{{{----------------------------------------------------------------
-def main(argv):
-    pass
+def main():
+    # some WIP code that maybe raises an exception
+    raise BaseException("oh no, exception!")
+    return 0
+
+
 #-}}}----------------------------------------------------------------------
 
-if __name__ == '__main__': main(sys.argv[1:]); sys.exit(0)
-# --- EOF ---------------------------------------------------------------------
+if __name__ == "__main__":
+    try:
+        ret = main()
+    except:
+        traceback.print_exc()
+        pdb.post_mortem()
+    sys.exit(ret)
+# --- EOF -----------------------------------------------------------------
